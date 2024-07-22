673994c5c2798f94d66e8999a31bf6ec7694b0c7
Olga Seleznova <olgaseleznova@Olgas-MacBook-Pro.local>
2024-07-21 17:16:20 -0700

    save_url, splitters research

---
+++ saveUrl.py
@@ -0,0 +1,98 @@
+from llama_index.llms.ollama import Ollama
+from llama_index.core import SummaryIndex
+# from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.readers.web import SimpleWebPageReader
+import os
+from langchain_text_splitters import HTMLSectionSplitter
+from langchain_community.document_loaders import AsyncChromiumLoader
+import re
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+from langchain_text_splitters import SpacyTextSplitter
+
+# import logging
+# import sys
+
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+
+def main_llamaindex(url):
+    llm = Ollama(model="llama3")
+    documents = SimpleWebPageReader(html_to_text=True).load_data([url])
+    index = SummaryIndex.from_documents(documents)
+    # set Logging to DEBUG for more detailed outputs
+    query_engine = index.as_query_engine(llm=llm)
+    response = query_engine.query("What are the job requirements?")
+    print(response)
+
+
+def load_html(url_):
+    loader = AsyncChromiumLoader(url_)
+    html = loader.load()
+    headers = [get_header(h.page_content) for h in html]
+    return html, headers
+
+
+def html_transform(my_html, tags_to_extract):
+    bs_transformer = BeautifulSoupTransformer()
+    docs_transf = bs_transformer.transform_documents(my_html)
+    return docs_transf
+
+
+def get_header(htmlTxt):
+    title = re.search(r'<title>(.*?)</title>', htmlTxt).group(1)
+    return re.sub(r'\W+', '', title)
+
+def get_digit(mystr):
+    digits = list(filter(lambda x: x.isdigit(), list(mystr)))
+    if len(digits)>0:
+        return int(digits[0])
+    else:
+        return 0
+
+def fileExists(fileName):
+    if os.path.exists(fileName):
+        dig = get_digit(fileName)
+        fileName = fileName.split(".txt")[0].removesuffix(str(dig)) + str(dig+1) +".txt"
+        if os.path.exists(fileName):
+            return fileExists(fileName)
+        else:
+            return fileName
+    else:
+        return fileName
+
+
+def main(url, outDir):
+    html, headers = load_html(url)
+    docs = html_transform(html, tags_to_extract=["span", ""])
+    for ind in range(len(docs)):
+        content = docs[ind].page_content
+        print(f"Content length is {len(content)}")
+        fileName = os.path.join(outDir,headers[ind]+".txt")
+        outFile = fileExists(fileName)
+        f = open(outFile, "w")
+        f.write(content)
+        f.close()
+
+    # chunkSem = semantic_split_docs(content)
+    # chunksChar = char_splitter(content)
+    # chunkTok = token_splitter(content)
+    # chunkSpacy = spacy_splitter(content)
+    # chunkSentTr = token_sent_trans(content)
+
+
+
+if __name__=="__main__":
+    main(
+        url=[
+            # "https://www.accenture.com/ca-en/careers/jobdetails?id=R00218148_en&title=Responsible%20AI%20Engineer%20SpecialistD",
+             "https://jobs.ashbyhq.com/quora/71e1b41f-ce2f-4144-a3b4-00c7d40f4e44?locationId=d03c8cce-ce51-4c89-9d06-ad8597402fc0"],
+        outDir = "posting/"
+    )
---
+++ splitters.py
@@ -0,0 +1,65 @@
+
+# SPLITTERS
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+from langchain_text_splitters import SpacyTextSplitter
+from langchain_text_splitters import TokenTextSplitter
+
+import en_core_web_sm
+
+# nlp = en_core_web_sm.load()
+
+def semantic_split(text):
+    text_splitter = SemanticChunker(OpenAIEmbeddings())
+    chunks = text_splitter.split_text(text)
+    print(f"Number of chunks: {len(chunks)}")
+    return chunks
+
+
+def char_splitter(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+        is_separator_regex=False)
+    chunks = text_splitter.split_text(text)
+    print(f"Number of chunks: {len(chunks)}")
+    return chunks
+
+
+def recursive_splitter(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 300, chunk_overlap=0)
+    chunks = text_splitter.split_text(text)
+    print(f"Number of chunks: {len(chunks)}")
+    return chunks
+
+
+def spacy_splitter(text):
+    text_splitter = SpacyTextSplitter(chunk_size=1000)
+    chunks = text_splitter.split_text(text)
+    print(f"Number of chunks: {len(chunks)}")
+    return chunks
+
+
+def main(file_name, outputFile):
+
+    f = open(file_name, "r")
+    content = f.read()
+    # chunks = semantic_split(content)
+    # chunks = char_splitter(content)
+    chunks = recursive_splitter(content)
+    # chunks = spacy_splitter(content)
+    f = open(outputFile, "w")
+    f.write("\n\n".join(chunks))
+    f.close()
+
+
+if __name__=="__main__":
+    main(
+        file_name = "posting/SeniorDataScientistPoeRemoteQuora.txt",
+        outputFile = "chunks_extracted/SeniorDataScientistPoeRemoteQuora_recursive_splitter.txt"
+    )
